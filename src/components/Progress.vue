adbdfa007d7b45bdb3104ba5ad9e3ab460b0fcc8
Loic <loic.wong@hotmail.com>
2018-08-09 17:40:17 +1200

    new file:   src/components/Progress.vue 	new file:   src/components/Step.vue

---
+++ src/components/Progress.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .steps
+    Step(v-for="step,index in steps" :step="step" :key="index" :isContainerWidth="false")
+</template>
+<script>
+import Step from '@/components/Step'
+export default{
+  props:['steps'],
+  components: {
+    Step
+  }
+}
+</script>
