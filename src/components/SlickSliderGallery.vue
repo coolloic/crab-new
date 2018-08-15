dd5bf286b62cd59c0aafc218d2e997f31ee5ff4b
Loic <loic.wong@hotmail.com>
2018-08-15 14:11:25 +1200

    modified:   src/components/SlickSliderGallery.vue

--- src/components/SlickSliderGallery.vue
+++ src/components/SlickSliderGallery.vue
@@ -54,37 +54,26 @@
 
       // Events listeners
       handleAfterChange(event, slick, currentSlide) {
-          console.log('handleAfterChange', event, slick, currentSlide);
       },
       handleBeforeChange(event, slick, currentSlide, nextSlide) {
-          console.log('handleBeforeChange', event, slick, currentSlide, nextSlide);
       },
       handleBreakpoint(event, slick, breakpoint) {
-          console.log('handleBreakpoint', event, slick, breakpoint);
       },
       handleDestroy(event, slick) {
-          console.log('handleDestroy', event, slick);
       },
       handleEdge(event, slick, direction) {
-          console.log('handleEdge', event, slick, direction);
       },
       handleInit(event, slick) {
-          console.log('handleInit', event, slick);
       },
       handleReInit(event, slick) {
-          console.log('handleReInit', event, slick);
       },
       handleSetPosition(event, slick) {
-          console.log('handleSetPosition', event, slick);
       },
       handleSwipe(event, slick, direction) {
-          console.log('handleSwipe', event, slick, direction);
       },
       handleLazyLoaded(event, slick, image, imageSource) {
-          console.log('handleLazyLoaded', event, slick, image, imageSource);
       },
       handleLazeLoadError(event, slick, image, imageSource) {
-          console.log('handleLazeLoadError', event, slick, image, imageSource);
       }
     }
   }
